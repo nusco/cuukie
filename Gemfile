5409485d4598e8bbc93dfe009dbf66a0730cff93
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2011-11-27 13:19:04 +0100

    fix multiple features test

--- Gemfile
+++ Gemfile
@@ -5,4 +5,5 @@ gem 'sinatra'
 group :test do
   gem 'rspec'
   gem 'rack-test'
+  gem 'cucumber' # FIXME: will move to the main group
 end
--- spec/show_features.rb
+++ spec/show_features.rb
@@ -15,18 +15,28 @@ describe 'The Cuukie server' do
   it "shows a home page" do
     get '/'
     last_response.should be_ok
-    last_response.body.should match 'Cucumber Features'
+    last_response.body.should match '<h1>Cucumber Features</h1>'
+    last_response.body.should match '<title>Cuukie</title>'
   end
-
+  
   it "cleans up all features at the beginning of a run" do
     post '/feature_name', {'name' => 'Create User'}.to_json
     post '/before_features'
 
     get '/'
-    last_response.body.should_not match 'Feature: Create User'
+    last_response.body.should_not match 'Create User'
   end
 
-  it "shows the keywords of features" do
+  it "shows multiple features" do
+    post '/feature_name', {'keyword' => 'Feature', 'name' => 'Create User'}.to_json
+    post '/feature_name', {'keyword' => 'Feature', 'name' => 'Delete User'}.to_json
+
+    get '/'
+    last_response.body.should match 'Feature: Create User'
+    last_response.body.should match 'Feature: Delete User'
+  end
+
+  it "shows the names and keywords of features" do
     post '/feature_name', {'keyword' => 'Use Case', 'name' => 'Create User'}.to_json
 
     get '/'
